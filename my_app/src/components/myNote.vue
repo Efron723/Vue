6207aa94a979cbd0f08b6a910b0e200d5c05991f
Efron <a161026521@gmail.com>
2024-08-05 14:58:11 +0800

    add :style & :class

--- my_app/src/components/myNote.vue
+++ my_app/src/components/myNote.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onUpdated, onUnmounted } from 'vue';
-const note = ref('My Note')
+const note = ref('')
 const textStyle = ref({ color: 'tomato', fontSize: '26px', fontWeight: 'bolder' })
 onUpdated(() => {
   updateLocalStorage()
@@ -20,9 +20,22 @@ const updateLocalStorage = () => {
 <template>
   <h1>我的Note</h1>
   <input v-model="note" type="text" placeholder="請輸入筆記內容" />
-  <p v-bind:style="textStyle">
+  <p :style="textStyle">
     我的筆記: {{ note }}
+  </p>
+  <p :class=note>
+    {{ 50 * 10 }}
   </p>
 </template>
 
-<style scoped></style>
+<style scoped>
+.font50 {
+  color: aquamarine;
+  font-size: 50px;
+}
+
+.font40 {
+  color: lightpink;
+  font-size: 40px;
+}
+</style>
